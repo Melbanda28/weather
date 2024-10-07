1328f5ba3ab049699c7fd1ee077b57e719bd3d7e
Melbanda28 <122828898+Melbanda28@users.noreply.github.com>
2024-10-07 18:43:30 +0000

    edited and updated  some code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
+import Weather from "./Weather";
+import "./App.css";
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
+    <div className="Container">
+    
     <div className="App">
       <header className="App-header">
-       hello Mel
+        <h1>Weather App</h1>
+        <Weather />
+
+        <footer>
+          This was coded by
+          <a
+            href="https://www.shecodes.io/graduates/123422-mellania-banda"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mellania Banda
+          </a>
+          , hosted on
+          <a
+            href="https://github.com/Melbanda28/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Git Hub
+          </a>
+          and website hosted on
+          <a
+            href="https://weather-mel-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Netlify
+          </a>
+        </footer>
       </header>
+    </div>
     </div>
   );
 }
 
-export default App;
